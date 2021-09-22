267a11d0b26efb84f962877bd54716f9368a86bc
Moisés J <moisesjbc@gmail.com>
2021-09-22 16:38:05 +0100

    Añadido generator (+ deteccion por parte del jugador)

--- godot-project/gameplay/player/player.gd
+++ godot-project/gameplay/player/player.gd
@@ -4,10 +4,13 @@ extends KinematicBody2D
 export (int) var movement_speed = 500
 export var hp = 3
 
+var close_generator = null
+
 
 func _physics_process(delta):
 	_process_player_movement(delta)
 	_process_player_rotation()
+	_process_player_actions()
 
 
 func _process_player_movement(delta):
@@ -27,8 +30,25 @@ func _process_player_movement(delta):
 
 func _process_player_rotation():
 	look_at(get_global_mouse_position())
+	
+	
+func _process_player_actions():
+	if close_generator and Input.is_action_just_pressed('ui_action'):
+		print('Cable!')
+		
 
 
 func damage():
 	hp -= 1
 	print('damage! new hp', hp)
+
+
+func _on_influence_area_body_entered(body):
+	print(body.get_groups())
+	if 'generators' in body.get_groups():
+		close_generator = body
+
+
+func _on_influence_area_body_exited(body):
+	if 'generators' in body.get_groups():
+		close_generator = null
