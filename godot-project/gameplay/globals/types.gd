caa8eacbbc2276fa922c61e3190f154e78ab1176
Moisés J <moisesjbc@gmail.com>
2021-09-24 19:57:06 +0100

    Elimina acciones de soltar y coger cable

--- godot-project/gameplay/globals/types.gd
+++ godot-project/gameplay/globals/types.gd
@@ -1,23 +1,5 @@
 extends Node
 
-class Context:
-	var player: Node2D
-	var player_actions: Node2D
-	var closest_interactuable: Node2D
-	var connections: Node2D
-	
-	
-	static func create(new_player, new_player_actions, new_closest_interactuable, new_connections):
-		var context = Context.new()
-
-		context.player = new_player
-		context.player_actions = new_player_actions
-		context.closest_interactuable = new_closest_interactuable
-		context.connections = new_connections
-		
-		return context
-
-
 enum IngameMenu {
   GAME_OVER,
   VICTORY
--- godot-project/gameplay/player/player.gd
+++ godot-project/gameplay/player/player.gd
@@ -17,7 +17,6 @@ func _ready():
 func _physics_process(delta):
 	_process_player_movement(delta)
 	_process_player_rotation()
-	_process_player_actions()
 
 
 func _process_player_movement(delta):
@@ -37,15 +36,6 @@ func _process_player_movement(delta):
 
 func _process_player_rotation():
 	look_at(get_global_mouse_position())
-	
-	
-func _process_player_actions():
-	var player_actions = get_parent().get_node('player_actions')
-
-	if Input.is_action_just_pressed('ui_action') and not player_actions.visible:
-		var context = Types.Context.create(self, player_actions, _get_closest_interactuable(), get_parent().get_node('connections'))
-		player_actions.init(context)
-		player_actions.global_position = global_position
 
 
 func _get_closest_interactuable():
