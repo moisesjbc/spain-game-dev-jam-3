4d96972e143fb8f31e579f57228230a8e97a2a50
Moisés J <moisesjbc@gmail.com>
2021-09-25 11:36:57 +0100

    Arregla warning de variable no usada

--- godot-project/menus/main_menu/main_menu.gd
+++ godot-project/menus/main_menu/main_menu.gd
@@ -6,7 +6,7 @@ func _ready():
 
 
 func _on_plain_button_pressed():
-	var erro = Utils.change_scene("res://gameplay/main/main.tscn")
+	Utils.change_scene("res://gameplay/main/main.tscn")
 
 
 func _on_exit_button_pressed():
