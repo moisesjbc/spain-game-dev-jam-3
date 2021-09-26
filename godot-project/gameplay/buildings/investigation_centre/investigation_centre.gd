010d6c73c96f5d3b32c74f8926db60f4fae9a9d9
Moisés J <moisesjbc@gmail.com>
2021-09-26 11:53:22 +0100

    Ajusta puntos de investigación en base a un delta por unidad de energía

--- godot-project/gameplay/buildings/investigation_centre/investigation_centre.gd
+++ godot-project/gameplay/buildings/investigation_centre/investigation_centre.gd
@@ -7,8 +7,8 @@ signal investigation_points_updated
 var target_investigation_points = 100.0
 var current_investigation_points = 0.0
 
-var max_investigation_points_per_second = 2.0
 var min_investigation_points_per_second = 0.5
+var investigation_points_improvement_per_energy_unit = 0.2
 
 var energy = 0
 var max_energy = 5
@@ -19,7 +19,7 @@ func _ready():
 
 
 func _on_investigation_timer_timeout():
-	var investigation_points = min_investigation_points_per_second + (energy / max_energy) * (max_investigation_points_per_second - min_investigation_points_per_second)
+	var investigation_points = min_investigation_points_per_second + energy * investigation_points_improvement_per_energy_unit
 	current_investigation_points += investigation_points
 	
 	if current_investigation_points >= target_investigation_points:
