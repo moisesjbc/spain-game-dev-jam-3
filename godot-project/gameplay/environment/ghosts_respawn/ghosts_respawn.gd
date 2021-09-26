5bdbc0f511c4b034a5a40462c4ec5161499a4311
Moisés J <moisesjbc@gmail.com>
2021-09-26 16:26:43 +0100

    Incrementa la dificultad con el tiempo

--- godot-project/gameplay/environment/ghosts_respawn/ghosts_respawn.gd
+++ godot-project/gameplay/environment/ghosts_respawn/ghosts_respawn.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var ghost_scene = preload("res://gameplay/ghost/ghost.tscn")
+var extra_speed = 0
 
 
 func _ready():
@@ -18,7 +19,12 @@ func respawn_ghost():
 	
 	get_tree().get_root().get_node('main').add_child(ghost)
 	ghost.global_position = $path/path_follow.global_position
+	ghost.movement_speed += extra_speed
 
 
 func _on_cooldown_timer_timeout():
 	respawn_ghost()
+
+
+func _on_difficulty_increment_timer_timeout():
+	extra_speed += 5
